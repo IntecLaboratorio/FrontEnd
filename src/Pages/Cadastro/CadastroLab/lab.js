35869dd43777f272b507d5960c720e8933d38504
Gustavo Augusto Wustemberg <gustavowustemberg14@gmail.com>
2022-10-01 15:08:09 -0300

    alter cadastro_lab

--- src/Pages/Cadastro/CadastroLab/lab.js
+++ src/Pages/Cadastro/CadastroLab/lab.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Table from 'react-bootstrap/Table';
 import NavCadastro from '../../../Components/NavCadastro';
 import '../cadastro.css';
 import api from '../../../Service/api.js'
+
 
 function Lab() {
 
@@ -10,6 +11,16 @@ function Lab() {
     const [name_lab, setName_lab] = useState("");
     const [room_index, setRoom_index] = useState("");
     const [floor_lab, setFloor_lab] = useState("");
+    const [labs, setLabs] = useState([]);
+
+    useEffect(() => {
+        async function getLabs() {
+            const { data } = await api.get('/labs');
+            setLabs(data)
+        }
+        getLabs();
+    }, []);
+
 
 
     async function insertLab() {
@@ -18,7 +29,7 @@ function Lab() {
 
             await api.post('/labs', data);
 
-            alert(`Patrimono ${name_lab} cadastrado com sucesso!`)
+            alert(` ${name_lab} cadastrado com sucesso!`)
 
         } catch (err) {
             alert(`Houve um problema: ${err}`)
@@ -32,7 +43,7 @@ function Lab() {
             <NavCadastro />
             <section floor_labName="container-cadastro secoes">
                 <Table striped bordered hover>
-                    <thead>
+                    <thead style={{ textAlign: "center" }}>
                         <tr>
                             <th>Instituição</th>
                             <th>Nome lab ou sala</th>
@@ -41,24 +52,16 @@ function Lab() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                        {labs.map((labs) => (
+                            <tr key={labs.id}>
+                                <td> {labs.fk_instruction} </td>
+                                <td> {labs.room_index} </td>
+                                <td> {labs.name_lab} </td>
+                                <td> {labs.floor_lab} </td>
+                            </tr>
+                        ))
+                        }
+
                     </tbody>
                 </Table>
             </section>
@@ -77,6 +80,25 @@ function Lab() {
                         </div>
 
                         <div className="wrap-input">
+
+                            <select name="select"
+                                className={room_index !== "" ? "has-val input" : "input"}
+                                type="text"
+                                value={room_index}
+                                onChange={(e) => setRoom_index(e.target.value)}
+                            >
+                                <option value="" disable selected></option>
+                                <option value="Laboratório">Laboratório</option>
+                                <option value="Sala de aula ">Sala de aula</option>
+                            </select>
+
+                            <span className="focus-input" data-placeholder="Tipo de sala"></span>
+                        </div>
+
+                    </section>
+                    <section className="section-cadastro">
+
+                        <div className="wrap-input">
                             <input
                                 className={name_lab !== "" ? "has-val input" : "input"}
                                 type="text"
@@ -85,38 +107,40 @@ function Lab() {
                             />
                             <span className="focus-input" data-placeholder="Nome do lab ou sala"></span>
                         </div>
-                    </section>
-                    <section className="section-cadastro">
                         <div className="wrap-input">
-                            <input
-                                className={room_index !== "" ? "has-val input" : "input"}
-                                type="text"
-                                value={room_index}
-                                onChange={(e) => setRoom_index(e.target.value)} />
-                            <span className="focus-input" data-placeholder="Tipo de sala"></span>
-                        </div>
-
-                        <div className="wrap-input">
-                            <input
+                            {/* <input
                                 className={floor_lab !== "" ? "has-val input" : "input"}
                                 type="text"
                                 value={floor_lab}
                                 onChange={(e) => setFloor_lab(e.target.value)}
-                            />
+                            /> */}
+                            <select name="select"
+                                className={floor_lab !== "" ? "has-val input" : "input"}
+                                type="text"
+                                value={floor_lab}
+                                onChange={(e) => setFloor_lab(e.target.value)}
+                            >
+                                <option value="" disable selected></option>
+                                <option value="1º andar">1º andar</option>
+                                <option value="2º andar">2º andar</option>
+                            </select>
+
                             <span className="focus-input" data-placeholder="Andar"></span>
                         </div>
                     </section>
+
                     <section className="section-btn-cadastro">
                         <button className="btn" onClick={insertLab}>Cadastrar</button>
                         <button className="btn">Alterar</button>
                     </section>
                 </form>
 
-
             </div>
         </div>
 
+
     );
+
 }
 
 export default Lab;
