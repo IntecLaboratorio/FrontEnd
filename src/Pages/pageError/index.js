487daf2edb343cbdfaa55cd42af95e83f1796b70
MathewsSmicaluk <mathews.git@gmail.com>
2022-10-28 13:56:10 -0300

    add tela de pagina não encontrada

--- src/Pages/pageError/index.js
+++ src/Pages/pageError/index.js
@@ -1,8 +1,25 @@
 import React from "react";
+import './style.css'
+import { Link } from 'react-router-dom';
 
 function app() {
     return(
-        <h1>Page não encontrada!</h1>
+        <>
+        <div className="flex">
+            <div className="container-error">
+                <h1 className="text-top">Ops!</h1>
+                <h2 className="text-bottom">Pagina não encontrada!</h2>
+
+                <Link to="/home">
+                    <button className="btn-error" >Voltar</button>
+                </Link>
+            </div>
+
+            <div className="img-error">
+                <img src="https://img.icons8.com/pastel-glyph/400/EFEFEF/page-not-found--v2.png"/>
+            </div>
+        </div>
+        </>
     )
 }
 
