748d4e9f9f08cef3470b56c1144c413c69931352
Guilherme Silva <guilhermesribeiro05@gmail.com>
2022-11-18 19:54:54 -0300

    mod table req lab

--- src/Components/TesteTable/index.jsx
+++ src/Components/TesteTable/index.jsx
@@ -1,50 +1,58 @@
 import { CTable } from "@coreui/react";
 import './style.css'
+import api from '../../Service/api.js';
+import { useEffect, useState } from "react";
+
 
 function Table() {
 
-    return (
-      <>
-    <table border="1" className="table-container">
+  const [reqLabs, setReqLabs] = useState([]);
+
+  useEffect(() => {
+    async function findReqLabs() {
+      const { data } = await api.get('/reqLabs');
+      setReqLabs(data)
+      console.log(data)
+    }
+    findReqLabs();
+  }, [reqLabs]);
+
+
+  return (
+    <>
+      <table border="1" className="table-container">
         <thead>
           <tr>
-              <th className="top">header1</th>
-              <th className="top">header2</th>
-              <th className="top">header3</th>
-              <th className="top">header4</th>
+            <th className="top">header1</th>
+            <th className="top">header2</th>
+            <th className="top">header3</th>
+            <th className="top">header4</th>
+            <th className="top">Aceitas?</th>
           </tr>
         </thead>
 
         <tbody>
-          <tr>
-              <td>Lab1</td>
-              <td></td>
-              <td></td>
-              <td>                <button className="btn-accept">Aceitar</button>
-                <button className="btn-deny">Negar</button></td>
-          </tr>
-
-          <tr>
-              <td>Lab2</td>
-              <td></td>
-              <td></td>
-              <td>                <button className="btn-accept">Aceitar</button>
-                <button className="btn-deny">Negar</button></td>
-          </tr>
-
-          <tr>
-              <td>Lab3</td>
-              <td></td>
-              <td></td>
-              <td>                <button className="btn-accept">Aceitar</button>
-                <button className="btn-deny">Negar</button></td>
-          </tr>
+          {
+            reqLabs.map((reqLab) => (
+              reqLabs.verify == 0 ?
+              <tr key={reqLab.id}>
+                <td>{reqLab.fk_discipline}</td>
+                <td>{reqLab.bloco_aula}</td>
+                <td>{reqLab.periodo}</td>
+                <td>{reqLab.data_req}</td>
+                <td>
+                  <button className="btn-accept">Aceitar</button>
+                  <button className="btn-deny">Negar</button>
+                </td>
+              </tr> : null
+            ))
+          }
         </tbody>
 
       </table>
 
-      </>
-    )
+    </>
+  )
 }
 
 export default Table;
