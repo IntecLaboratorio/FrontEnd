e8f91c24a2a8bc03f05e78905f17cb906316eba3
marianaBadu <marianabadu5@gmail.com>
2022-11-20 18:24:39 -0300

    alt tela solicitação de lab e alterar senha

--- src/Components/Table/index.jsx
+++ src/Components/Table/index.jsx
@@ -3,40 +3,53 @@ import Table from 'react-bootstrap/Table';
 import './Style.css';
 
 function Index(props) {
-  return (
-    <div className='table-container'>
-       <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>header1</th>
-                            <th>header2</th>
-                            <th>header3</th>
-                            <th>header4</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </Table>
-    </div>
-  );
+    
+    const [date, setDate] = useState("");
+
+    return (
+        <div className='table-container'>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>
+                            <input
+                                className={date !== "" ? "has-val input" : "input"}
+                                type="text"
+                                value={date}
+                                onChange={(e) => setDate(e.target.value)}
+                            />
+                            <span
+                                className="focus-input"
+                                data-placeholder="Data"
+                            ></span>
+
+                        </th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </Table>
+        </div>
+    );
 }
 
 export default Index;
---
+++ src/Components/TrocarSenha/trocaSenha.js
@@ -0,0 +1,151 @@
+import { Button, Modal, Spinner } from "react-bootstrap";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./style.css";
+import api from "../../Service/api.js";
+import { useNavigate } from "react-router-dom";
+
+function TrocarSenha() {
+    const [senha, setsenha] = useState("");
+    const [confirmSenha, setConfirmsenha] = useState("");
+    const handleShow = () => setShow(true);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [loading, setLoading] = useState("");
+    let navigate = useNavigate();
+
+    const validate = () => {
+        if (!senha && !confirmSenha) {
+            toast.warn("Preencha os dois campos!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            return false;
+        }
+        if (!senha) {
+            toast.warn("Preencha sua senha atual", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            return false;
+        }
+        if (!confirmSenha) {
+            toast.warn("Confirme sua senha atual", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            return false;
+        }
+        if (senha != confirmSenha) {
+            toast.warn("As senhas não são iguais", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            return false;
+        }
+        return true;
+    };
+
+    const updatePassword = async (e) => {
+        e.preventDefault();
+        if (validate()) {
+            try {
+                setLoading(<Spinner id="loading" animation="border" />);
+                setIsDisabled(true);
+
+                const email = sessionStorage.getItem("email");
+                const dados = { email, senha };
+                await api.post("/passwordUser", dados);
+
+
+                setLoading("");
+                handleClose();
+                navigate("*");
+
+            } catch (err) {
+                setIsDisabled(false);
+                setLoading("");
+                console.log(err);
+                toast.error("Senha incorreta!", {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            }
+        }
+    };
+    return (
+        <>
+
+            <Modal show={show} onHide={() => setShow(false)} className="modal">
+                <Modal.Body>
+                    <section>
+                        <div className="wrap-input">
+                            <input
+                                className={senha !== "" ? "has-val input" : "input"}
+                                type="senha"
+                                value={senha}
+                                onChange={(e) => setsenha(e.target.value)}
+                            />
+                            <span className="focus-input" data-placeholder="Nova Senha"></span>
+                        </div>
+
+                        <div className="wrap-input">
+                            <input
+                                className={confirmSenha !== "" ? "has-val input" : "input"}
+                                type="email"
+                                value={confirmSenha}
+                                onChange={(e) => setConfirmsenha(e.target.value)}
+                            />
+                            <span
+                                className="focus-input"
+                                data-placeholder="Confirme sua Senha"
+                            ></span>
+                        </div>
+
+                        <div className="loading-password">{loading}</div>
+
+                        <div className="container-login-form-btn">
+                            <button
+                                className="btn-trocarSenha"
+                                onClick={updatePassword}
+                                disabled={isDisabled}
+                            >
+                                Trocar Senha
+                            </button>
+                        </div>
+                    </section>
+                </Modal.Body>
+            </Modal>
+            <ToastContainer />
+        </>
+    );
+}
+
+export default TrocarSenha;
